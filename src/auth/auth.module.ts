4e80c02e8e1214b49a70d4d1dd95ab7672ffe2bf
Leandro Arturi <lea.arturi@gmail.com>
2024-04-11 11:20:46 -0300

    Login with JWT

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,8 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
+  imports: [
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get('JWT_SECRET'),
+        signOptions: { expiresIn: '60m' },
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,14 +1,25 @@
 import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import { LoginDto } from './dtos/login.dto';
 
 @Injectable()
 export class AuthService {
+  constructor(private jwtService: JwtService) {}
+
   async login(loginDto: LoginDto) {
-    const login =
+    const isValidLogin =
       loginDto.email === process.env.AUTH_USER && loginDto.password === process.env.AUTH_PASSWORD;
 
+    if (!isValidLogin) {
+      return { login: false };
+    }
+
+    const payload = { email: loginDto.email, sub: loginDto.email };
+    const token = this.jwtService.sign(payload);
+
     return {
-      login,
+      login: true,
+      access_token: token,
     };
   }
 }
